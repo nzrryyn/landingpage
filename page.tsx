e24972953df5b3623c3ffaf7bf0adfe2097e03f3
nzrryyn <nizardpa@gmail.com>
2025-09-30 19:53:19 +0800

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import './[landingpage]/itulah.css'
+
+
+export default function Landing() {
+  return (
+    <div className="background">
+      <nav className="navbar">
+        <h2>Home</h2>
+        <h2>About</h2>
+        <h2>Contact</h2>
+        <h2>ID ML</h2>
+      </nav>
+    
+      <div className="content">
+       
+        <h1>Hello, saya Nizar Royyan.</h1>
+        <h1>Dari XI Frontend</h1>
+        <h2>yang mau mabar 200k/match</h2>
+        <div><button>More about me</button></div>
+          <div className='gambar'>
+      <img src="2 1.jpg" alt="" /> </div>
+      </div>
+
+      <div className='content2'>
+        <div><h1>About me</h1></div>
+        <div><p>I'll be the one that stays 'til the end
+And Ill be the one that needs you again
+And I'll be the one that proposes in a garden of roses
+And truly loves you long after our curtain closes
+But will you still love me when nobody wants me around?
+When I turn 81 and forget things, will you still be proud?
+'Cause I am the one who's waited this long
+And I am the one that might get it wrong
+And I'll be the one that will love you the way I'm supposed to
+Girl, oh</p><div><button>More about me</button></div>
+ <div className='gambar2'>
+  <img src="yus 1.jpg" alt="" />
+</div>
+
+
+ 
+<div className='content3'>
+ <div><h1>My Services</h1></div>
+  <div className='kotak'>
+    <h2>UI/UX Designer</h2>
+    <p>asdoahdsoahsdaslk</p>
+    <button>contact me</button>
+  </div>
+
+  <div className='kotak'>
+    <h2>Fotografi</h2>
+    <p>asdajsdoiasjdoajsdojasodj</p>
+    <button>contact me</button>
+  </div>
+    <div className='kotak'><h2>Frontend DEV</h2>
+    <p>kakshdlaskjd</p>
+    <button>contact me</button>
+    </div>
+  
+
+</div>
+</div>
+</div>
+</div>
+  )
+}
